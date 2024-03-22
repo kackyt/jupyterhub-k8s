3365eb201a3c5fe4d510df9e25bb2830f9733e7f
kacky <t_kaki@nextappli.com>
2024-03-22 19:34:28 +0900

    fix: jupyter serverを起動

--- docker/lab/run.sh
+++ docker/lab/run.sh
@@ -3,5 +3,5 @@
 source .rye/env
 source .venv/bin/activate
 
-jupyterhub-singleuser
-# jupyter lab --no-browser --ip "*" --port 9000 --allow-root --NotebookApp.token=''
+# jupyter lab --no-browser --ip "*" --ServerApp.port=8888 --allow-root --NotebookApp.token=''
+jupyter server --no-browser --ip "*" --allow-root --NotebookApp.token=''
