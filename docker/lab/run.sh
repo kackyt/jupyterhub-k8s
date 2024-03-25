70190f6e7ecd12bdbc8758cfed74987a5afdb816
kacky <t_kaki@nextappli.com>
2024-03-25 19:02:27 +0900

    fix: run.shを起動しない不具合を修正

--- docker/lab/run.sh
+++ docker/lab/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-source .rye/env
-source .venv/bin/activate
-
 # jupyter lab --no-browser --ip "*" --ServerApp.port=8888 --allow-root --NotebookApp.token=''
 jupyter server --no-browser --ip "*" --allow-root --NotebookApp.token=''
