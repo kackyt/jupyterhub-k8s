4b20aebb92c4f0070c948ecb9a63c36ae66b5b39
kacky <t_kaki@nextappli.com>
2024-03-30 18:47:41 +0900

    feat: torchなどのインストールテスト

--- docker/lab/build.sh
+++ docker/lab/build.sh
@@ -2,16 +2,17 @@
 
 set -e
 
+curl -LsSf https://astral.sh/uv/install.sh | sh
 curl -sSf https://rye-up.com/get | RYE_NO_AUTO_INSTALL=1 RYE_INSTALL_OPTION="--yes" bash
 
 . $HOME/.rye/env
 
-rye config --set-bool behavior.use-uv=true
 rye sync --no-dev
 
 . .venv/bin/activate
 
 jupyter lab build --dev-build=False --minimize=False
 
+echo "source $HOME/.cargo/env" >> $HOME/.bash_profile
 echo "source $HOME/.rye/env" >> $HOME/.bash_profile
 echo "source .venv/bin/activate" >> $HOME/.bash_profile
