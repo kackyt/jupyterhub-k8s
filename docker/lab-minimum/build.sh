bf03f458c4f19c4ba06bce32db68ce055795e2f3
kacky <t_kaki@nextappli.com>
2024-07-21 12:27:19 +0900

    refactor: レビュー対応

--- docker/lab-minimum/build.sh
+++ docker/lab-minimum/build.sh
@@ -3,16 +3,18 @@
 set -e
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-curl -sSf https://rye-up.com/get | RYE_NO_AUTO_INSTALL=1 RYE_INSTALL_OPTION="--yes" bash
+curl -sSf https://rye.astral.sh/get | RYE_NO_AUTO_INSTALL=1 RYE_INSTALL_OPTION="--yes" bash
 
-. $HOME/.rye/env
+. "$HOME/.rye/env"
 
 rye sync --no-dev
 
-. .venv/bin/activate
+. ".venv/bin/activate"
 
 jupyter lab build --dev-build=False --minimize=False
 
-echo "source $HOME/.cargo/env" >> $HOME/.bash_profile
-echo "source $HOME/.rye/env" >> $HOME/.bash_profile
-echo "source .venv/bin/activate" >> $HOME/.bash_profile
+{
+echo "source $HOME/.cargo/env"
+echo "source $HOME/.rye/env"
+echo "source .venv/bin/activate"
+} >> $HOME/.bash_profile
--- docker/lab/build.sh
+++ docker/lab/build.sh
@@ -3,15 +3,15 @@
 set -e
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-curl -sSf https://rye-up.com/get | RYE_TOOLCHAIN_VERSION="3.12" RYE_INSTALL_OPTION="--yes" bash
+curl -sSf https://rye.astral.sh/get | RYE_TOOLCHAIN_VERSION="3.12" RYE_INSTALL_OPTION="--yes" bash
 
-. $HOME/.cargo/env
-. $HOME/.rye/env
+. "$HOME/.cargo/env"
+. "$HOME/.rye/env"
 
 rye config --set-bool behavior.use-uv=true
 HTTP_TIMEOUT=86400 rye sync --no-dev --no-lock
 
-. .venv/bin/activate
+. ".venv/bin/activate"
 
 # rye config --set-bool behavior.use-uv=false
 # rye add xformers
@@ -22,6 +22,8 @@ HTTP_TIMEOUT=86400 rye sync --no-dev --no-lock
 
 jupyter lab build --dev-build=False --minimize=False
 
-echo "source $HOME/.cargo/env" >> $HOME/.bash_profile
-echo "source $HOME/.rye/env" >> $HOME/.bash_profile
-echo "source .venv/bin/activate" >> $HOME/.bash_profile
+{
+echo "source $HOME/.cargo/env"
+echo "source $HOME/.rye/env"
+echo "source .venv/bin/activate"
+} >> $HOME/.bash_profile
